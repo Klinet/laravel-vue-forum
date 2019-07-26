f9725d9ce8c8709a2dd4c9353e37d5b3c4cb1698
Klinet <akali.klinet@gmail.com>
2019-07-26 23:02:18 +0200

    question updated with user and date

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController');
+
+//Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
